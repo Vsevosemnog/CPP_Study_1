b5a0f842379dfaceb2e79f60f58e8198f9a9c70e
Vsevosemnog <esmitin17@mail.ru>
2023-05-17 21:41:15 +0300

    work with project

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,164 @@
 #include <iostream>
 #include <vector>
+#include <bitset>
+#include <assert.h>
+
+namespace Core
+{
+    namespace Util
+    {
+        bool isLittleEndian()
+        {
+            //0x00 0x00 0x00 0b0000 0101
+            int a = 5;
+            std::string result = std::bitset<0>(a).to_string();
+            if (result.back() == '1') return true;
+        }
+    }
+    // 0 1 2 3
+    // 0x00 0x00 0x00 0x5 >> LE
+    template <typename T>
+    void encode(std::vector<int8_t>* buffer, int16_t* iterator, T value)
+    {
+        for (unsigned i = 0; i < sizeof(T) ; i++)
+        {
+            //(*buffer)[(*iterator)++] = value  >> ((sizeof(T) * 8) - 8 - ((i == 0) ? j : j += 8)); 
+            //(*buffer)[(*iterator)++] = value  >> ((sizeof(T) * 8) - 8 - i*8); 
+            (*buffer)[(*iterator)++] = value  >> 8 * (sizeof(T) - i); 
+        }
+
+    }
+
+/*     void encode16(std::vector<int8_t>* buffer, int16_t* iterator, int16_t value)
+    {
+        (*buffer)[(*iterator)++] = value >> 8;
+        (*buffer)[(*iterator)++] = value;
+    } */
+
+}
+
+//Java-like object model
+namespace ObjectModel
+{
+    enum class Wrapper: int8_t
+    {
+        PRIMITIVE=1,
+        ARRAY,
+        STRING,
+        OBJECT
+    };    
+
+    enum class Type: int8_t
+    {
+        I8=1,
+        I16,
+        I32,
+        I64,
+
+        U8,
+        U16,
+        U32,
+        U64,
+
+        FLOAT,
+        DOUBLE,
+
+        BOOL
+    };
+    class Root
+    {
+    protected:
+        std::string name;
+        int16_t nameLength;
+        int8_t wrapper;
+        int32_t size;
+        Root();  
+    public:
+        int32_t getSize();
+        void setName(std::string);
+        std::string getName();
+        virtual void pack(std::vector<int8_t>*, int16_t*);
+        //virtual void pack(); // = 0; //pure virtual function 
+        
+    };
+    class Primitive : public Root
+    {
+    private:
+        int8_t type = 0;
+        std::vector<int8_t>* data = nullptr;
+    private:
+        Primitive();
+    public:
+        //abstract factory? builder method?
+        static Primitive* createI32(std::string name, Type type, int32_t value);
+        //static Primitive* createI32(std::string name, Type type, int32_t value);
+        //static Primitive* createI64(std::string name, Type type, int64_t value);
+        void pack(std::vector<int8_t>*, int16_t*);
+
+    };
+    class Array : public Root
+    {
+
+    };
+    class Object : public Root
+    {
+
+    };
+
+    //definition
+
+    Root::Root()
+        :
+        name("unknown"),
+        wrapper(0),
+        nameLength(0),
+        size(sizeof nameLength + sizeof wrapper + sizeof size) {}
+
+    void Root::setName(std::string name)
+    {
+        this->name = name;
+        this->nameLength = static_cast<int16_t>(name.length());
+        size += nameLength;
+    }
+
+    int32_t Root::getSize()
+    {
+        return size;
+    }
+
+    void Root::pack(std::vector<int8_t>*, int16_t*)
+    {
+        //
+    }
+
+    std::string Root::getName()
+    {
+        return name;
+    }
+
+    Primitive::Primitive()
+    {
+        size += sizeof type;
+    }
+
+    Primitive* Primitive::createI32(std::string name, Type type, int32_t value)
+    {
+        Primitive* p = new Primitive();
+
+        p->setName(name);
+        p->wrapper= static_cast<int8_t>(Wrapper::PRIMITIVE);
+        p->type=static_cast<int8_t>(type);
+        p->data = new std::vector<int8_t>(sizeof value);
+        int16_t iterator = 0;
+        Core::encode(p->data, &iterator, value);
+
+    }
+
+    void Primitive::pack(std::vector<int8_t>*, int16_t*)
+    {
+
+    }
+}
 
 namespace EventSystem
 {
@@ -19,6 +178,7 @@ namespace EventSystem
         void addEvent(Event*);
         Event* getEvent();
         bool isActive();
+        void serialize();
     };
 
     class Event
@@ -78,6 +238,10 @@ namespace EventSystem
         e->bind(this,e);
     }
 
+    void System::serialize()
+    {
+        
+    }
     Event* System::getEvent()
     {
         return events.front();
@@ -116,12 +280,24 @@ namespace EventSystem
 
 
 using namespace EventSystem;
+using namespace ObjectModel;
 
 int main(int argc, char** argv)
 {
-
+    assert(Core::Util::isLittleEndian());
+    int32_t foo = 5;
+    Primitive* p = Primitive::createI32("int32", Type::I32, foo);
+#if 0
     System foo("foo");
     Event* e = new KeyboardEvent('a',true, false);
+    //Event* e1 = new KeyboardEvent('b',true, false);
+
+    foo.addEvent(e);
+    
+    KeyboardEvent* kb = static_cast<KeyboardEvent*>(foo.getEvent());
+    foo.serialize();
+    //foo.addEvent(e1);
+#endif
     (void)argc;
     (void)argv;
     return 0;
